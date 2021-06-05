7e1020f2d61ad2affb3c0acdfc0af17405861cc9
Rich Heller <rellehr@gmail.com>
2021-06-05 17:46:03 -0500

    Initial setup

---
+++ 2019/18/Part1.cs
@@ -0,0 +1,11 @@
+namespace aoc.y2019.day18
+{
+    class Part1 : aoc.utils.ProblemSolver<int>
+    {
+        public Part1(string file, int exp) : base(file, exp) { }
+
+        protected override int doWork() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
