64fce79d349844e11692fb4ae592820faeac8436
Rich Heller <rellehr@gmail.com>
2021-06-17 23:18:58 -0500

    Fix runner

--- Runner.cs
+++ Runner.cs
@@ -47,6 +47,7 @@ namespace aoc_csharp
             new aoc.y2019.day19.Part1("input/2019/19/puzzle.txt", 234),
             new aoc.y2019.day19.Part2("input/2019/19/puzzle.txt", 9290812),
             new aoc.y2019.day20.Part1("input/2019/20/puzzle.txt", 526),
+            new aoc.y2019.day20.Part2("input/2019/20/puzzle.txt", 6292),
           };
 
         static void runAll(List<Problem> toRun) {
@@ -73,7 +74,7 @@ namespace aoc_csharp
         }
 
         static void Main(string[] args) {
-            var probs = new List<Problem>() { new aoc.y2019.day20.Part2("input/2019/20/puzzle.txt", 6292) };
+            // var probs = new List<Problem>() { new aoc.y2019.day20.Part2("input/2019/20/puzzle.txt", 6292) };
 
             runAll(probs);
         }
