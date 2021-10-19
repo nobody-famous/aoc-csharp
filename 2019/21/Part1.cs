25ee7ec6ee39c27a420ea6b4aaa6ddb91951796b
Rich Heller <rellehr@gmail.com>
2021-10-18 23:58:57 -0500

    Refactor code

--- 2019/21/Part1.cs
+++ 2019/21/Part1.cs
@@ -1,70 +1,15 @@
-using aoc.y2019.intcode;
-
 namespace aoc.y2019.day21
 {
-    class Part1 : aoc.utils.ProblemSolver<int>
+    class Part1 : Solver
     {
         public Part1(string fileName, int exp) : base(fileName, exp) { }
 
-        private void ReadPrompt(Controller ctrl) {
-            var prompt = ctrl.ReadString();
-
-            if (prompt != "Input instructions:") {
-                throw new System.Exception($"Invalid prompt: {prompt}");
-            }
+        override protected string getSpeed() {
+            return "WALK";
         }
 
-        private void SendCommands(Controller ctrl, string[] cmds) {
-            foreach (var cmd in cmds) {
-                ctrl.WriteString(cmd);
-            }
-
-            ctrl.WriteString("WALK");
-        }
-
-        private void ExpectString(Controller ctrl, string str) {
-            var value = ctrl.ReadString();
-
-            if (str != value) {
-                throw new System.Exception($"Expected {str}, found {value}");
-            }
-        }
-
-        private void SkipWalkingMsg(Controller ctrl) {
-            ExpectString(ctrl, "");
-            ExpectString(ctrl, "Walking...");
-            ExpectString(ctrl, "");
-        }
-
-        private void SkipStatusMsg(Controller ctrl) {
-            ExpectString(ctrl, "Didn't make it across:");
-            ExpectString(ctrl, "");
-        }
-
-        private bool ShowFrame(Controller ctrl) {
-            var lastFrame = false;
-            var line = "";
-
-            do {
-                line = ctrl.ReadString();
-                lastFrame = lastFrame || (line.Length > 0 && line[0] == '#' && line.Contains('@'));
-                System.Console.WriteLine(line);
-            } while (line.Length > 0);
-
-            return lastFrame;
-        }
-
-        private void WalkFailed(Controller ctrl) {
-            var lastFrame = false;
-            while (!lastFrame) {
-                lastFrame = ShowFrame(ctrl);
-            }
-        }
-
-        protected override int doWork() {
-            var prog = Parser.parseInput(inputFile);
-            var ctrl = new Controller(prog);
-            var cmds = new string[] {
+        protected override string[] getCommands() {
+            return new string[] {
                 "NOT A J",
                 "NOT B T",
                 "OR T J",
@@ -72,21 +17,6 @@ namespace aoc.y2019.day21
                 "OR T J",
                 "AND D J",
              };
-
-            ReadPrompt(ctrl);
-            SendCommands(ctrl, cmds);
-
-            SkipWalkingMsg(ctrl);
-
-            var value = ctrl.ReadValue();
-
-            if (value < 256) {
-                SkipStatusMsg(ctrl);
-                WalkFailed(ctrl);
-                throw new System.Exception("FAILED");
-            }
-
-            return (int)value;
         }
     }
 }
--- 2019/21/Utils.cs
+++ 2019/21/Utils.cs
@@ -2,6 +2,90 @@ using aoc.y2019.intcode;
 
 namespace aoc.y2019.day21
 {
+    abstract class Solver : aoc.utils.ProblemSolver<int>
+    {
+        public Solver(string fileName, int exp) : base(fileName, exp) { }
+
+        abstract protected string getSpeed();
+        abstract protected string[] getCommands();
+
+        private void ReadPrompt(Controller ctrl) {
+            var prompt = ctrl.ReadString();
+
+            if (prompt != "Input instructions:") {
+                throw new System.Exception($"Invalid prompt: {prompt}");
+            }
+        }
+
+        private void SendCommands(Controller ctrl, string[] cmds) {
+            foreach (var cmd in cmds) {
+                ctrl.WriteString(cmd);
+            }
+        }
+
+        private void ExpectString(Controller ctrl, string str) {
+            var value = ctrl.ReadString();
+
+            if (str != value) {
+                throw new System.Exception($"Expected {str}, found {value}");
+            }
+        }
+
+        private void SkipMsg(Controller ctrl, string msg) {
+            ExpectString(ctrl, "");
+            ExpectString(ctrl, $"{msg}...");
+            ExpectString(ctrl, "");
+        }
+
+        private void SkipStatusMsg(Controller ctrl) {
+            ExpectString(ctrl, "Didn't make it across:");
+            ExpectString(ctrl, "");
+        }
+
+        private bool ShowFrame(Controller ctrl) {
+            var lastFrame = false;
+            var line = "";
+
+            do {
+                line = ctrl.ReadString();
+                lastFrame = lastFrame || (line.Length > 0 && line[0] == '#' && line.Contains('@'));
+                System.Console.WriteLine(line);
+            } while (line.Length > 0);
+
+            return lastFrame;
+        }
+
+        private void WalkFailed(Controller ctrl) {
+            var lastFrame = false;
+            while (!lastFrame) {
+                lastFrame = ShowFrame(ctrl);
+            }
+        }
+
+        protected override int doWork() {
+            var prog = Parser.parseInput(inputFile);
+            var ctrl = new Controller(prog);
+            var speed = getSpeed();
+            var cmds = getCommands();
+
+            ReadPrompt(ctrl);
+            SendCommands(ctrl, cmds);
+
+            ctrl.WriteString(speed);
+            SkipMsg(ctrl, speed == "WALK" ? "Walking" : "Running");
+
+            var value = ctrl.ReadValue();
+
+            if (value < 256) {
+                SkipStatusMsg(ctrl);
+                WalkFailed(ctrl);
+                throw new System.Exception("FAILED");
+            }
+
+            return (int)value;
+        }
+    }
+
     class Controller : IOHandler
     {
         private Machine machine;
